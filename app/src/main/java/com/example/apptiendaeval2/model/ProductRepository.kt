f6473df0b859b24ca3b4550ff8fd80be4ff25c0e
MooaaY <di.medinao@duocuc.cl>
2025-10-26 20:07:27 -0300

    Cambios en el ProductRepository.kt de nombre, valoracion de clientes y Descripcion.

--- app/src/main/java/com/example/apptiendaeval2/model/ProductRepository.kt
+++ app/src/main/java/com/example/apptiendaeval2/model/ProductRepository.kt
@@ -6,171 +6,171 @@ object ProductRepository {
     private val products = listOf(
         Producto(
             id = 1,
-            nombre = "Polera Negra",
+            nombre = "Polera Negra Forever",
             precio = 12990,
-            descripcion = "Polera de algodón 100%, cómoda. aumentará tu aura ",
+            descripcion = "Polera de algodon 100%, comoda. aumentara tu aura ",
             imagenResId = R.drawable.polera_negra,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("María", 5, "Muy buena calidad."),
-                Valoracion("Joaquín", 4, "Cómoda, llegó rápido goood.")
+                Valoracion("MariaUwU", 5, "Muy buena calidad para mi novio jiji."),
+                Valoracion("ElJuacoInsano", 4, "Comoda, llego rapido goood.")
             )
         ),
         Producto(
             id = 2,
-            nombre = "Polera azul",
+            nombre = "Polera Azul Los Angeles",
             precio = 15000,
-            descripcion = "Polera de algodon 100% y muy comoda.",
+            descripcion = "Polera de algodon 100% UnderGround.",
             imagenResId = R.drawable.polera_azul,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Carlos", 4, "Cómoda, me quedó de pana mi rey."),
+                Valoracion("Carlillos777", 4, "Comoda, me quedo de pana mi rey."),
                 Valoracion("momo", 4, "muy buena, vaneado pa.")
             )
         ),
         Producto(
             id = 3,
-            nombre = "polera blanca",
+            nombre = "Polera Blanca Skyline",
             precio = 12990,
-            descripcion = "Polera de poliester y comoda para usar",
+            descripcion = "Polera de poliester de RyF Pegado",
             imagenResId = R.drawable.polera_gris,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("martin",5,"está muy buena, la recomiendo chat"),
-                Valoracion("santiago", 4, "el material muy bueno y la polera fiel a al foto.")
+                Valoracion("Ferguson",5,"está muy buena, la recomiendo chat"),
+                Valoracion("Hater12", 2, "El disenio estamu y mal porke estA pegao dislaics")
             )
         ),
         Producto(
             id = 4,
-            nombre = "poleron cropped gris",
+            nombre = "Poleron Cropped Gris",
             precio = 25000,
-            descripcion = "poleron con gorro corte cropped",
+            descripcion = "poleron con capucha cropped Intervalans",
             imagenResId = R.drawable.poleron_gris,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("daniel",6,"muy buen producto, y la calidad es un 10"),
-                Valoracion("moaay",4,"no me quedó bien amiguis, como lo devuelvo?")
+                Valoracion("Corazun7.7",4,"muy buen producto, y la calidad es un 10"),
+                Valoracion("MooaaY",4,"no me quedó bien amiguis, como lo devuelvo?")
             )
         ),
         Producto(
             id = 5,
-            nombre = "Polera azul marino",
+            nombre = "Polera Azul Marino Suki",
             precio = 12990,
             descripcion = "camiseta de manga corta con cuello redondo y estampado",
             imagenResId = R.drawable.polera_auto,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Papi Micky",4, "aceptan canje o no?"),
-                Valoracion("Jh", 2, "me quedó grande muchachos")
+                Valoracion("Papi Micky",3, "aceptan canje o no?"),
+                Valoracion("Jh", 2, "me quedo grande muchachos")
             )
         ),
         Producto(
             id = 6,
-            nombre = "Polera gris de mujer",
+            nombre = "Polera Gris de Mujer StayWild",
             precio = 12990,
             descripcion = "camiseta de manga corta casual y vintage",
             imagenResId = R.drawable.polera_mujer,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("María",6, "muy bonitaa"),
-                Valoracion("constanza", 5, "me quedó super, la volveria a comprar")
+                Valoracion("RataPeluda",5, "muy bonitaa"),
+                Valoracion("NamiPlayer", 4, "me quedo super, la volveria a comprar 100 veces jjij")
             )
         ),
         Producto(
             id = 7,
-            nombre = "Polera morada",
+            nombre = "Polera Morada AsiantiK",
             precio = 12990,
-            descripcion = "Camiseta de algodón y estampado de dragón",
+            descripcion = "Camiseta de algodon y estampado de dragón",
             imagenResId = R.drawable.polera_dragon,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Vicente",6, "tal cual la imagen, me gustó mucho"),
-                Valoracion("Emilia", 5, "justo lo que esperaba, como siempre la calidad 10/10")
+                Valoracion("VichoCo66",6, "tal cual la imagen, me gusto mucho"),
+                Valoracion("Emili<3", 5, "justo lo que esperaba y la calidad 10/10")
             )
         ),
         Producto(
             id = 8,
-            nombre = "Polera gris y negro",
+            nombre = "Polera Gris y Negro",
             precio = 12990,
             descripcion = "Camiseta de hombre con cuello redondo manga corta",
             imagenResId = R.drawable.polera_negro_gris,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Rodrigo",6, "muy buena calidad"),
-                Valoracion("Benjamin", 5, "la tela me gustó")
+                Valoracion("Kakashi69",4, "muy buena calidad me gustoooo"),
+                Valoracion("Benjita6.5", 5, "ta entera wena la polera siii")
             )
         ),
         Producto(
             id = 9,
-            nombre = "Polera gris y negro",
+            nombre = "Polera Gris y Negro AsiantiK",
             precio = 12990,
             descripcion = "Camiseta de hombre street de manga corta y estampado",
             imagenResId = R.drawable.polera_negra_estampado,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Manuel",6, "muy linda y comoda"),
-                Valoracion("Carlos", 5, "la pedí en L y me quedó super")
+                Valoracion("Manucraft4",5, "muy linda y comoda"),
+                Valoracion("FerguinZ", 4, "la pedí en L y me llego tal cual (soy gei)")
             )
         ),
         Producto(
             id = 10,
-            nombre = "Polera Grunge Punk",
+            nombre = "Polera Grunge Punk Phantom",
             precio = 12990,
             descripcion = "Camiseta con estampado de silueta de hombre",
             imagenResId = R.drawable.polera_negra_dark,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Mateo",6, "me encantó el diseño, está muy bonita la verdad"),
-                Valoracion("Lucas", 5, "recomendadaaaa")
+                Valoracion("Alishin",3, "me encanto el diseño y no juego lol con los panas"),
+                Valoracion("LaParka", 4, "recomendadaaaa 100%%%%")
             )
         ),
         Producto(
             id = 11,
-            nombre = "Jeans grises",
+            nombre = "Jeans vaquero Gris",
             precio = 12990,
-            descripcion = "Jeans rectos de pierna ancha para hombre, estilo casual de calle",
+            descripcion = "Jeans rectos de pierna ancha para hombre, estilo vaquero",
             imagenResId = R.drawable.pantalon_gris,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("José",6, "un 10/10"),
-                Valoracion("Thiago", 5, "muy buena ropa")
+                Valoracion("MontaCaballos",3, "muy buena pero la calidad meh"),
+                Valoracion("HogRiders", 4, "muy buena ropa osi osi")
             )
         ),
         Producto(
             id = 12,
-            nombre = "Camiseta casual",
+            nombre = "Camiseta Casual Gris AsiantiK",
             precio = 12990,
             descripcion = "Camiseta gris de hombre con diseño de dragon",
             imagenResId = R.drawable.camista_diseno_dragon,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Hector",6, "fresca la tela, me gustó"),
-                Valoracion("Maxi", 5, "está buena")
+                Valoracion("Ironman",3, "fresca la tela, me gusto"),
+                Valoracion("Dr.Straño", 3, "esta buena")
             )
         ),
         Producto(
             id = 13,
-            nombre = "Pantalon corto",
+            nombre = "Pantalon Corto",
             precio = 12990,
-            descripcion = "Shorts informales con estampado",
+            descripcion = "Shorts informales con estampado UnderGround",
             imagenResId = R.drawable.pantalon_corto,
             imagenesResId = listOf(
             ),
             valoraciones = listOf(
-                Valoracion("Gabriel",5, "El diseño es de lo mejor"),
-                Valoracion("Javier", 6, "calidad 10/10")
+                Valoracion("Anndy33",5, "El diseño es de lo mejor"),
+                Valoracion("Hater2", 1, "el estampao se me salio despue de lavar el shor")
             )
         )
 
